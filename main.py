a75cc6ee2474ef3dd58d3538b3535dc64ac4c9e5
Tomas Kriz <tomas.kriz15@email.cz>
2024-09-13 10:56:05 +0200

    Vytvoření bloku, pro zachycení výjimky v hlavním menu

--- main.py
+++ main.py
@@ -19,34 +19,47 @@ def league_choice(user_choice):
     return inner_menu
 
 def main_menu(my_con, my_cur):
-    print("Správa databáze")
-    print("[1] - Stažení/Aktualizace týmů v databázi: ")
-    print("[2] - Stažení hráčů, do databáze: ")
-    user_choice = int(input("Vyber následující hodnotu z nabídky: "))
+    while True:
+        try:
+            print("Správa databáze")
+            print("[1] - Stažení/Aktualizace týmů v databázi ")
+            print("[2] - Stažení hráčů, do databáze ")
+            print("[0] - Konec")
+            user_choice = int(input("Vyber následující hodnotu z nabídky: "))
 
-    if user_choice == 1:
-        inner_choice = league_choice(user_choice)
+            if user_choice < 0 or user_choice > 2:
+                print("Zadaná hodnota se nenachází v nabídce!")
 
-        dtb_returned_leagues = sql_queries.get_data_simple(my_cur, choosen_table="leagues")
-        scraped_teams = data_downloader.teams_download(dtb_returned_leagues, inner_choice)
-        dtb_teams = sql_queries.get_data_simple(my_cur, choosen_table="teams")
+            elif user_choice == 1:
+                inner_choice = league_choice(user_choice)
 
-        teams_duplicity_object = duplicity_checker.DuplicityChecker(dtb_teams, scraped_teams)
-        team_duplicity_result = teams_duplicity_object.dtb_duplicity_check()
-        if team_duplicity_result is not None:
-            sql_queries.insert_data(my_con, my_cur, "teams",["team_name", "league_id", "elite_url"], [team_duplicity_result.team_name, team_duplicity_result.league_id, team_duplicity_result.url])
+                dtb_returned_leagues = sql_queries.get_data_simple(my_cur, choosen_table="leagues")
+                scraped_teams = data_downloader.teams_download(dtb_returned_leagues, inner_choice)
+                dtb_teams = sql_queries.get_data_simple(my_cur, choosen_table="teams")
 
-    elif user_choice == 2:
-        inner_choice = league_choice(user_choice)
+                teams_duplicity_object = duplicity_checker.DuplicityChecker(dtb_teams, scraped_teams)
+                team_duplicity_result = teams_duplicity_object.dtb_duplicity_check()
+                if team_duplicity_result is not None:
+                    sql_queries.insert_data(my_con, my_cur, "teams",["team_name", "league_id", "elite_url"], [team_duplicity_result.team_name, team_duplicity_result.league_id, team_duplicity_result.url])
 
-        dtb_returned_teams = sql_queries.get_data_simple(my_cur, choosen_table="teams")
-        scraped_players = data_downloader.players_url_download(dtb_returned_teams, inner_choice)
+            elif user_choice == 2:
+                inner_choice = league_choice(user_choice)
 
-        dtb_returned_players = sql_queries.get_data_simple(my_cur, choosen_table="players")
-        players_duplicity_object = duplicity_checker.DuplicityChecker(dtb_returned_players, scraped_players, my_con=my_con, my_cur = my_cur)
-        p_duplicity_result = players_duplicity_object.dtb_duplicity_check(table="players")
-        if p_duplicity_result is not None:
-            sql_queries.insert_data(my_con, my_cur, "players", [p_duplicity_result.surname, p_duplicity_result.last_name, p_duplicity_result.nationality, p_duplicity_result.league_id, p_duplicity_result.player_position, p_duplicity_result.date_of_birth, p_duplicity_result.team_id, p_duplicity_result.url])
+                dtb_returned_teams = sql_queries.get_data_simple(my_cur, choosen_table="teams")
+                scraped_players = data_downloader.players_url_download(dtb_returned_teams, inner_choice)
+
+                dtb_returned_players = sql_queries.get_data_simple(my_cur, choosen_table="players")
+                players_duplicity_object = duplicity_checker.DuplicityChecker(dtb_returned_players, scraped_players, my_con=my_con, my_cur = my_cur)
+                p_duplicity_result = players_duplicity_object.dtb_duplicity_check(table="players")
+                if p_duplicity_result is not None:
+                    sql_queries.insert_data(my_con, my_cur, "players", ["surname", "last_name", "nationality", "league_id", "player_position", "date_of_birth", "team_id", "elite_url"],[p_duplicity_result.surname, p_duplicity_result.last_name, p_duplicity_result.nationality, p_duplicity_result.league_id, p_duplicity_result.player_position, p_duplicity_result.date_of_birth, p_duplicity_result.team_id, p_duplicity_result.url])
+
+            elif user_choice == 0:
+                print("Neplecha ukončena!")
+                break
+
+        except ValueError as e:
+            print("Zadat můžeš pouze číslo!")
 
 if __name__ == "__main__":
     load_dotenv("dev.env")
